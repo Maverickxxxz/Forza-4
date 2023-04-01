e3c1b14fa5c6ac603ae8dcd9ee9579ff1357028c
Maverickxxxz <blackmaverickpsn@gmail.com>
2023-04-01 13:00:57 +0200

    committ

--- Login/registrazione.js
+++ Login/registrazione.js
@@ -1,28 +1,37 @@
-function clickLogin(numero){
+function clickLogin(numero, reg){
+
     let bottoneRegistrati = document.getElementById("bottoneRegistrati");
     let campo_nome = document.getElementById("campo_nome");
     let titolo = document.getElementById("titolo");
     let bottoneLogin = document.getElementById("bottoneLogin");
+    let stato = reg;
 
+    if(stato=='false'){
+        campo_nome.style.maxHeight = 0; //Cancella il campo nome
+        titolo.innerHTML = "Accedi";
+        bottoneRegistrati.classList.add("disabilitato");
+        bottoneLogin.classList.remove("disabilitato");
+    }
+
+    if(stato=='true'){
+        campo_nome.style.maxHeight = 0; //Cancella il campo nome
+        titolo.innerHTML = "Accedi";
+        bottoneRegistrati.classList.add("disabilitato");
+        bottoneLogin.classList.remove("disabilitato");
+        
+    }
 
     if(numero==1){
-       
-    campo_nome.style.maxHeight = 0; //Cancella il campo nome
-    titolo.innerHTML = "Accedi";
-    bottoneRegistrati.classList.add("disabilitato");
-    bottoneLogin.classList.remove("disabilitato");
+       stato = 'false';
     }
 
     if(numero==0){
-       
-        campo_nome.style.maxHeight = "60px"; //Cancella il campo nome
-        titolo.innerHTML = "Registrati";
-        bottoneRegistrati.classList.remove("disabilitato");
-        bottoneLogin.classList.add("disabilitato");
+        stato = 'true';
         }
 
        
 }
+
 function presa_elementi_registrazione(){
     let nome_utente = document.form_registrazione.nome_utente.value;
     let email = document.form_registrazione.email.value;
