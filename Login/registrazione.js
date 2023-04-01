0bcb6063d691401e37a297d3c2651c41cf189856
Maverickxxxz <blackmaverickpsn@gmail.com>
2023-04-01 10:45:51 +0200

    First Commit

---
+++ Login/registrazione.js
@@ -0,0 +1,24 @@
+function presa_elementi_registrazione(){
+    let nome_utente = document.form_registrazione.nome_utente.value;
+    let email = document.form_registrazione.email.value;
+    let password = document.form_registrazione.password.value;  
+
+    var inviato = false;
+
+
+    //CONTROLLA SE I CAMPI SONO VUOTI
+    if(nome_utente==""){
+        alert("Non hai inserito il nome utente!");
+        return false;
+    }
+
+    if(email==""){
+        alert("Non hai inserito l'email!");
+        return false;
+    }
+
+    if(password==""){
+        alert("Non hai inserito la password!");
+        return false;
+    }
+}
