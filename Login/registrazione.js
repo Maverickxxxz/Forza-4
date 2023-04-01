b75d057b8402378c9dd9207367448cabbf2c015e
Maverickxxxz <blackmaverickpsn@gmail.com>
2023-04-01 22:05:28 +0200

    Funzionamento login-signup

--- Login/registrazione.js
+++ Login/registrazione.js
@@ -1,39 +1,52 @@
-function clickLogin(numero, reg){
+
+
+const urlParams = new URLSearchParams(window.location.search);
+var stato = urlParams.get('reg');
+
+//CONTROLLA SE L'UTENTE HA CLICCATO LOGIN O REGISTRATI
+if(stato=="login"){
+    campo_nome.style.maxHeight = 0; //Cancella il campo nome
+    titolo.innerHTML = "Accedi";
+    bottoneRegistrati.classList.add("disabilitato");
+    bottoneLogin.classList.remove("disabilitato");
+}
+
+if(stato=="signup"){
+    campo_nome.style.maxHeight = "60px"; 
+    titolo.innerHTML = "Registrati";
+    bottoneRegistrati.classList.remove("disabilitato");
+    bottoneLogin.classList.add("disabilitato");
+    
+}
+
+
+
+
+    
+
+function clickLogin(numero){
 
     let bottoneRegistrati = document.getElementById("bottoneRegistrati");
     let campo_nome = document.getElementById("campo_nome");
     let titolo = document.getElementById("titolo");
     let bottoneLogin = document.getElementById("bottoneLogin");
-    let stato = reg;
 
-    if(stato=='false'){
+    if(numero==1){ //FORM LOGIN
         campo_nome.style.maxHeight = 0; //Cancella il campo nome
         titolo.innerHTML = "Accedi";
         bottoneRegistrati.classList.add("disabilitato");
         bottoneLogin.classList.remove("disabilitato");
     }
 
-    if(stato=='true'){
-        campo_nome.style.maxHeight = 0; //Cancella il campo nome
-        titolo.innerHTML = "Accedi";
-        bottoneRegistrati.classList.add("disabilitato");
-        bottoneLogin.classList.remove("disabilitato");
+    if(numero==0){ //FORM REGISTRAZIONE
+        campo_nome.style.maxHeight = "60px"; 
+        titolo.innerHTML = "Registrati";
+        bottoneRegistrati.classList.remove("disabilitato");
+        bottoneLogin.classList.add("disabilitato");
         
     }
 
-    if(numero==1){
-       stato = 'false';
-    }
-
-    if(numero==0){
-        stato = 'true';
-        }
 }  
-
-function prova(ciao){
-    var prova = ciao;
-    alert(prova);
-}
 
 function presa_elementi_registrazione(){
     let nome_utente = document.form_registrazione.nome_utente.value;
