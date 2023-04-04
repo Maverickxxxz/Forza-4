80e2093f473f7cf90750e853f12cbf30a356eef3
Maverickxxxz <blackmaverickpsn@gmail.com>
2023-04-04 23:53:41 +0200

    fixes, register form added

---
+++ Login/database.php
@@ -0,0 +1,20 @@
+<?php
+$host = "localhost";
+$dbname = "register_database";
+$username = "root";
+$password = "";
+
+$mysqli = new mysqli(hostname: $host, 
+                     username: $username,
+                     password: $password, 
+                     database: $dbname); 
+
+
+
+if($mysqli->connect_errno){ //SE C'è UN ERRORE DI CONNESSIONE MOSTRA L'ERRORE
+    die("Errore di connessione: " . $mysqli->connect_error); // IL PUNTO SERVE PER CONCATENARE DUE STRINGHE INSIEME
+}
+
+return $mysqli;
+
+?>
--- Login/signup-process.php
+++ Login/signup-process.php
@@ -1,5 +1,40 @@
 <?php
 
-echo("ciaoo");
+
+
+$nome_utente_r = $_POST["nome_utente_r"];
+$email_r = $_POST["email_r"];
+$password_r = $_POST["password_r"];
+$password_hash_r = password_hash($_POST["password_r"], PASSWORD_DEFAULT); //hashed, crittografata
+
+$mysqli = require __DIR__ . "/database.php";
+
+$sql = "INSERT INTO register_database . utente (nome_utente_r, email_r, password_hash_r)
+        VALUES (?, ?, ?)";
+        
+$stmt = $mysqli->stmt_init();
+
+if ( ! $stmt->prepare($sql)) {          //SE FALLISCE LA PREPARAZIONE DELLA QUERY SQL
+    die("ERRORE SQL: " . $mysqli->error);
+}
+
+$stmt->bind_param("sss",                //ASSOCIA AI VALORI ? ? ? RISPETTIVAMENTE, NAME, EMAIL E PASSWORD(hashed), sss specifica il tipo di dato delle variabili, tutte stringhe
+                  $_POST["nome_utente_r"],
+                  $_POST["email_r"],
+                  $password_hash_r);
+                  
+if ($stmt->execute()) { //VIENE ESEGUITA LA QUERY
+
+    header("Location: signup-success.html"); //L'UTENTE VIENE REINDIRIZZATO A QUESTA PAGINA
+    exit;
+    
+} else {
+    
+    if ($mysqli->error === "Uncaught mysqli_sql_exception") {      //KNOWN ERROR, VEDE SE LA MAIL è GIà IN USO
+        die("email già iscritta");
+    } else {
+        die($mysqli->error . " " . $mysqli->errno);
+    }
+}
 
 ?>
