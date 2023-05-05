2f27bbac70ede01c2735c7e270b856c94f75a0e6
Maverickxxxz <blackmaverickpsn@gmail.com>
2023-05-05 12:57:08 +0200

    socket

--- game/Multiplayer/client/script.js
+++ game/Multiplayer/client/script.js
@@ -1,3 +1,17 @@
- import {io} from 'socket.io-client'
+import { io } from 'socket.io-client';
 
- const socket = io("http://localhost:3000")
+const socket = io('http://localhost:3000');
+socket.on('connect', () => {
+  displayMessage(`Sei connesso con id: ${socket.id}`);
+  
+});
+
+function displayMessage(message) {
+  const messagesDiv = document.getElementById('messages');
+  const paragraph = document.createElement('p');
+  paragraph.textContent = message;
+  messagesDiv.appendChild(paragraph);
+}
+
+socket.emit("prova-server", 10, "ciao", {a: "a"}) //manda l'evento al server
+
--- game/Multiplayer/server/server.js
+++ game/Multiplayer/server/server.js
@@ -1,6 +1,14 @@
-const io = require('socket.io')(3000)
+const io = require('socket.io')(3000, {
+    cors: {
+        origin: ['http://localhost:8080'],
+    },
+})
+
 
 io.on('connection', socket => {
     console.log(socket.id)
+    socket.on("prova-server", (number,string,obj) =>{
+        console.log(number,string,obj)
+    })
 })
 
