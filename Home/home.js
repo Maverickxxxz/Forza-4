c49aa37d89831b27dd6389a3fb16aede0c1d4a38
mattiamaffo <maffongelli.1941528@studenti.uniroma1.it>
2023-05-10 19:29:57 +0200

    fuu

--- Home/home.js
+++ Home/home.js
@@ -69,36 +69,6 @@ $("#elem5").on({
     }
 })
 
-<<<<<<< HEAD
-=======
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
-};
-
-function revealFunction(){
-
-    window.sr = ScrollReveal({
-        distance: '65px',
-        duration: 2600,
-        delay: 450,
-        easing: 'ease-out'
-    });
-
-    
-    sr.reveal('.hero-text', {delay:200, origin:'left', reset:false});
-    sr.reveal('.hero-img', {delay:450, origin:'right', reset:false});
-    sr.reveal('.scroll-down', {delay:500, origin:'bottom', reset:false});
-    sr.reveal('.animato', {delay:450, origin:'top', reset:false});
-    sr.reveal('.navlist', {delay:450, origin:'top', reset:false});
-
-}
-
-window.addEventListener('load', () => {
-    revealFunction();    
-})
-
->>>>>>> bbd4fad45fbd3aad3b3023dc5d4f2d418720695b
 function menuToggle() {
     var img = document.getElementById("board");
     if(cont == 0){
