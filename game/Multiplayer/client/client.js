b3d2add12504cbf8493efbb6ce80db0cb3765809
Maverickxxxz <blackmaverickpsn@gmail.com>
2023-05-15 10:58:59 +0200

    sockets

--- game/Multiplayer/client/client.js
+++ game/Multiplayer/client/client.js
@@ -21,129 +21,131 @@ export function uniscitiStanza() {
   socket.emit("unisciti-stanza", nomeStanzaUnione, nome_utente);
 }
 
+
+
+function setWinner(r, c) {
+  let winner = document.getElementById("winner");
+  if (board[r][c] == playerRed) {
+      winner.innerText = "Red Wins";             
+  } else {
+      winner.innerText = "Yellow Wins";
+  }
+  gameOver = true;
+}
+
+function checkWinner() {
+  // horizontal
+  for (let r = 0; r < rows; r++) {
+      for (let c = 0; c < columns - 3; c++){
+         if (board[r][c] != ' ') {
+             if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                 setWinner(r, c);
+                 return;
+             }
+         }
+      }
+ }
+
+ // vertical
+ for (let c = 0; c < columns; c++) {
+     for (let r = 0; r < rows - 3; r++) {
+         if (board[r][c] != ' ') {
+             if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                 setWinner(r, c);
+                 return;
+             }
+         }
+     }
+ }
+
+ // anti diagonal
+ for (let r = 0; r < rows - 3; r++) {
+     for (let c = 0; c < columns - 3; c++) {
+         if (board[r][c] != ' ') {
+             if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                 setWinner(r, c);
+                 return;
+             }
+         }
+     }
+ }
+
+ // diagonal
+ for (let r = 3; r < rows; r++) {
+     for (let c = 0; c < columns - 3; c++) {
+         if (board[r][c] != ' ') {
+             if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                 setWinner(r, c);
+                 return;
+             }
+         }
+     }
+ }
+}
+
+function setPiece() {
+  if (gameOver) {
+      return;
+  }
+
+  //get coords of that tile clicked
+  let coords = this.id.split("-");
+  let r = parseInt(coords[0]);
+  let c = parseInt(coords[1]);
+
+  // figure out which row the current column should be on
+  r = currColumns[c]; 
+
+  if (r < 0) { // board[r][c] != ' '
+      return;
+  }
+
+  board[r][c] = currPlayer; //update JS board
+  let tile = document.getElementById(r.toString() + "-" + c.toString());
+  if (currPlayer == playerRed) {
+      tile.classList.add("red-piece");
+      currPlayer = playerYellow;
+  }
+  else {
+      tile.classList.add("yellow-piece");
+      currPlayer = playerRed;
+  }
+
+  r -= 1; //update the row height for that column
+  currColumns[c] = r; //update the array
+
+  checkWinner();
+}
+
+function setGame() {
+  let board = [];
+  let currColumns = [5, 5, 5, 5, 5, 5, 5];
+  let rows = 6;
+  let columns = 7;
+  let gameOver = false;
+
+  for (let r = 0; r < rows; r++) {
+      let row = [];
+      for (let c = 0; c < columns; c++) {
+          // JS
+          row.push(' ');
+          // HTML
+          let tile = document.createElement("div");
+          tile.id = r.toString() + "-" + c.toString();
+          tile.classList.add("tile");
+          tile.addEventListener("click", setPiece);
+          document.getElementById("board").append(tile);
+      }
+      board.push(row);
+  }
+}
+
+
+
 //CONNESSIONE AL SERVER CHE HA PORTA 3000
 const socket = io('http://localhost:3000');
 
 socket.on('connect', () => {
-
-    
-  
-    function setWinner(r, c) {
-      let winner = document.getElementById("winner");
-      if (board[r][c] == playerRed) {
-          winner.innerText = "Red Wins";             
-      } else {
-          winner.innerText = "Yellow Wins";
-      }
-      gameOver = true;
-    }
-  
-    function checkWinner() {
-      // horizontal
-      for (let r = 0; r < rows; r++) {
-          for (let c = 0; c < columns - 3; c++){
-             if (board[r][c] != ' ') {
-                 if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
-                     setWinner(r, c);
-                     return;
-                 }
-             }
-          }
-     }
-  
-     // vertical
-     for (let c = 0; c < columns; c++) {
-         for (let r = 0; r < rows - 3; r++) {
-             if (board[r][c] != ' ') {
-                 if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
-                     setWinner(r, c);
-                     return;
-                 }
-             }
-         }
-     }
-  
-     // anti diagonal
-     for (let r = 0; r < rows - 3; r++) {
-         for (let c = 0; c < columns - 3; c++) {
-             if (board[r][c] != ' ') {
-                 if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
-                     setWinner(r, c);
-                     return;
-                 }
-             }
-         }
-     }
-  
-     // diagonal
-     for (let r = 3; r < rows; r++) {
-         for (let c = 0; c < columns - 3; c++) {
-             if (board[r][c] != ' ') {
-                 if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
-                     setWinner(r, c);
-                     return;
-                 }
-             }
-         }
-     }
-  }
-  
-    function setPiece() {
-      if (gameOver) {
-          return;
-      }
-  
-      //get coords of that tile clicked
-      let coords = this.id.split("-");
-      let r = parseInt(coords[0]);
-      let c = parseInt(coords[1]);
-  
-      // figure out which row the current column should be on
-      r = currColumns[c]; 
-  
-      if (r < 0) { // board[r][c] != ' '
-          return;
-      }
-  
-      board[r][c] = currPlayer; //update JS board
-      let tile = document.getElementById(r.toString() + "-" + c.toString());
-      if (currPlayer == playerRed) {
-          tile.classList.add("red-piece");
-          currPlayer = playerYellow;
-      }
-      else {
-          tile.classList.add("yellow-piece");
-          currPlayer = playerRed;
-      }
-  
-      r -= 1; //update the row height for that column
-      currColumns[c] = r; //update the array
-  
-      checkWinner();
-  }
-  
-    function setGame() {
-      let board = [];
-      let currColumns = [5, 5, 5, 5, 5, 5, 5];
-      let rows = 6;
-      let columns = 7;
-      let gameOver = false;
-    
-      for (let r = 0; r < rows; r++) {
-          let row = [];
-          for (let c = 0; c < columns; c++) {
-              // JS
-              row.push(' ');
-              // HTML
-              let tile = document.createElement("div");
-              tile.id = r.toString() + "-" + c.toString();
-              tile.classList.add("tile");
-              tile.addEventListener("click", setPiece);
-              document.getElementById("board").append(tile);
-          }
-          board.push(row);
-      }
-    }
     
 
   const idUtenteElement = document.getElementById("id_utente");
@@ -174,8 +176,12 @@ socket.on('stanza-creata', (idStanza, nomeStanza) => {
 socket.on("naviga-a-gioco", (idStanza) => {
   const primaPagina = document.getElementById("prima-pagina");
   const secondaPagina = document.getElementById("seconda-pagina");
-  //primaPagina.style.display = "none";
-  secondaPagina.style.display = "block";
+   // Nascondo la prima pagina
+   primaPagina.style.display = "none";
+  
+   // Mostro la seconda pagina
+   secondaPagina.style.display = "block";
+  
   setGame();
 });
 
